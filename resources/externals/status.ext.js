4a01b60e18f13b00cbbeb704e16166cd598e35b3
Roman Volosovskyi <rasom@ukr.net>
2016-11-08 12:36:32 +0200

    transactions sending, production build

--- resources/externals/status.ext.js
+++ resources/externals/status.ext.js
@@ -1,3 +1,10 @@
-var statusApi = {
-    dispatch: function(){}
-};
+var statusAPI = {};
+statusAPI.dispatch = function(){};
+
+Web3.prototype.fromWei = function(){};
+Web3.prototype.toWei = function(){};
+
+Web3.eth = {};
+Web3.eth.getAccounts = function(){};
+Web3.eth.getBalance = function(){};
+Web3.eth.sendTransaction = function(){};
